a04f1ddec3ea7b230a6f639265e906afd999b64d
Toliska <68003133+toliska@users.noreply.github.com>
2025-05-30 17:25:12 +0300

    Update atm.c

--- main.c
+++ main.c
@@ -1,80 +1,112 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <time.h>
 
+#define MAX_NAME 50
+#define MAX_PIN 6
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
-int menu(){
-	char op1[] = "Deposit";
-	char op2[] = "Withdraw";
-	char op3[] = "Check Balance";
-	int answ;
-	system("cls");
-	printf("Select an option:\n1. %s\n2. %s\n3. %s\n",op1,op2,op3);
-	scanf(" %d",&answ);
-	system("cls");
-	return answ;
+typedef struct {
+    char name[MAX_NAME];
+    char pin[MAX_PIN];
+    int balance;
+} Account;
+
+void printLine() {
+    printf("===========================================\n");
 }
 
-void deposit(int*bal){
-	int dp;
-	printf("\t\t\tDeposit Menu\nBalance: %d EUR\n", *bal);
-	printf("Please insert the prefered amount for deposit: ");
-	scanf(" %d",&dp);
-	printf("Amount: %d has been succesfully deposited to the account\n",dp);
-	*bal += dp;
-	printf("New Balance: %d EUR\n",*bal);
+int menu() {
+    int answ;
+    printLine();
+    printf("ATM SYSTEM MENU\n");
+    printLine();
+    printf("1. Deposit\n2. Withdraw\n3. Check Balance\n4. Exit\n");
+    printLine();
+    printf("Select an option: ");
+    scanf("%d", &answ);
+    return answ;
 }
 
-void withdraw(int* bal){
-	int wd;
-	if (*bal == 0){
-		printf("Insufficient funds for withdraw\n");
-	}
-	else {
-		printf("Balance: %d EUR\nPlease insert the prefered amount for withdraw: ",*bal);
-		scanf("%d",&wd);
-		if (wd > *bal){
-			printf("Insufficient funds\n");			
-		}else {
-			printf("Amount: %d has been succesfully withdrawn from the account\n",wd);
-			*bal -= wd;
-			printf("New Balance: %d EUR\n",*bal);
-		}
-	}
-	
+void deposit(Account *acc) {
+    int dp;
+    printf("\n--- Deposit Menu ---\nBalance: %d EUR\n", acc->balance);
+    printf("Enter amount to deposit: ");
+    scanf("%d", &dp);
+    if (dp > 0) {
+        acc->balance += dp;
+        printf("Successfully deposited: %d EUR\nNew Balance: %d EUR\n", dp, acc->balance);
+    } else {
+        printf("Invalid deposit amount!\n");
+    }
 }
 
-void checkBalance(int* bal){
-	printf("Current balance: %d EUR\n",*bal);	
+void withdraw(Account *acc) {
+    int wd;
+    printf("\n--- Withdraw Menu ---\nBalance: %d EUR\n", acc->balance);
+    if (acc->balance == 0) {
+        printf("Insufficient funds.\n");
+        return;
+    }
+    printf("Enter amount to withdraw: ");
+    scanf("%d", &wd);
+    if (wd > acc->balance) {
+        printf("Insufficient funds.\n");
+    } else if (wd <= 0) {
+        printf("Invalid withdrawal amount!\n");
+    } else {
+        acc->balance -= wd;
+        printf("Successfully withdrew: %d EUR\nNew Balance: %d EUR\n", wd, acc->balance);
+    }
 }
 
-int main(int argc, char *argv[]) {
-	system("title ATM System");
-	char op1[] = "Deposit";
-	char op2[] = "Withdraw";
-	char op3[] = "Check Balance";
-	int answ;
-	int bal = 0;
-	int dp;
-	
-	char answ1 = 'y';
-	
-	while (answ1 == 'y'){
-		answ = menu();
-		if (answ == 1){
-			deposit(&bal);
-		}else if (answ == 2){
-			withdraw(&bal);			
-		}
-		else if (answ ==3){
-			checkBalance(&bal);
-		}
-		printf("\nWould you like to make another transaction (y/n)?");
-		scanf(" %c",&answ1);
-	}
-	system("cls");
-	printf("Thank you for using ATM!!!");		
-	return 0;
+void checkBalance(const Account *acc) {
+    printf("\n--- Account Balance ---\nAccount Holder: %s\nBalance: %d EUR\n", acc->name, acc->balance);
 }
 
+int authenticate(Account *acc) {
+    char input_pin[MAX_PIN];
+    printf("\nPlease enter your 4-digit PIN: ");
+    scanf("%s", input_pin);
+    if (strcmp(acc->pin, input_pin) == 0) {
+        printf("Authentication successful.\n");
+        return 1;
+    } else {
+        printf("Authentication failed.\n");
+        return 0;
+    }
+}
 
+int main() {
+    Account user = {"John Doe", "1234", 0};
+    system("title ATM System");
+
+    printf("Welcome to the Advanced ATM System\n");
+    if (!authenticate(&user)) {
+        printf("Exiting due to failed authentication.\n");
+        return 1;
+    }
+
+    int option;
+    do {
+        option = menu();
+        switch (option) {
+            case 1:
+                deposit(&user);
+                break;
+            case 2:
+                withdraw(&user);
+                break;
+            case 3:
+                checkBalance(&user);
+                break;
+            case 4:
+                printf("Thank you for using the ATM system!\n");
+                break;
+            default:
+                printf("Invalid option. Try again.\n");
+        }
+    } while (option != 4);
+
+    return 0;
+}
